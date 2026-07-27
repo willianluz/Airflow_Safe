bb71e393b9d90248587b7145510aede4d9d0cbb4
Willian Luz <willian.luz@safeweb.com.br>
2026-07-27 20:50:36 -0300

    Teste de dag

---
+++ DagTeste.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="minha_primeira_dag",
+    start_date=datetime(2026, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    tags=["teste"],
+) as dag:
+
+    tarefa = BashOperator(
+        task_id="dizer_ola",
+        bash_command="echo 'Olá do Airflow'",
+    )
